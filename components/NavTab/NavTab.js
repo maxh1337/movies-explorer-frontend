40524f459f6073edefb4e97432f7010bc15b9592
Maksim Vorobev <max11052003@yandex.ru>
2022-03-02 11:43:09 +0300

    hopewarwillendsoon

---
+++ components/NavTab/NavTab.js
@@ -0,0 +1,13 @@
+import './NavTab.css';
+
+function NavTab() {
+  return (
+    <section className="nav-tab">
+      <a href="#О_проекте" className="nav-tab__link">О проекте</a>
+      <a href="#Технологии" className="nav-tab__link">Технологии</a>
+      <a href="#Студент" className="nav-tab__link">Студент</a>
+    </section>
+  );
+}
+
+export default NavTab;
