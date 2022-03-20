b366a28474e9271e1bc14d42cae319173f2f3bdc
Maksim Vorobev <max11052003@yandex.ru>
2022-03-20 13:52:48 +0300

    gfg

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -219,7 +219,7 @@ function App() {
       .then(() => {
         setLoggedIn(false);
         setCurrentUser({});
-        setSavedMovies([]);
+        setSavedMovies([]); 
         // TODO возможно придётся убрать
         history.push('/');
       })
